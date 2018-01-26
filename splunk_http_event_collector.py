3393682193339aedfbfcc0a93a7b3fc116bc90f8
George Starcher <george@georgestarcher.com>
2018-01-26 16:51:37 -0600

    Make the HEC payload post try twice and pass any exceptions to keep threads from locking if a failed connection or other issues interfere with the requests post.

--- splunk_http_event_collector.py
+++ splunk_http_event_collector.py
@@ -136,7 +136,16 @@ class http_event_collector:
                 print "Events received on thread. Sending to Splunk."
             payload = " ".join(self.flushQueue.get())
             headers = {'Authorization':'Splunk '+self.token}
-            r = requests.post(self.server_uri, data=payload, headers=headers, verify=self.http_event_collector_SSL_verify)
+            # try to post payload twice then give up and move on
+            try:
+                r = requests.post(self.server_uri, data=payload, headers=headers, verify=self.http_event_collector_SSL_verify)
+            except Exception as e:
+                pass
+            try:
+                r = requests.post(self.server_uri, data=payload, headers=headers, verify=self.http_event_collector_SSL_verify)
+            except Exception as e:
+                pass
+
             if self.http_event_collector_debug:
                 print r.text
             self.flushQueue.task_done()
