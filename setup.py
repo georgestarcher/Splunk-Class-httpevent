7c6b7cffc019706cbd5ecb838c93fe2fb07df10e
George Starcher <george@georgestarcher.com>
2018-03-17 00:37:52 -0500

    update setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,17 @@
 
 from distutils.core import setup
 
+def readme():
+    with open('readme.md') as f:
+        return f.read()
+
 setup(name='Splunk-HEC',
       version='1.5',
       description='This is a python class file for use with other python scripts to send events to a Splunk http event collector.',
+      long_description=readme(),
       author='George (starcher) Starcher',
       author_email='george@georgestarcher.com',
-      url='https://www.splunk.com/blog/2015/12/11/http-event-collect-a-python-class/',
+      url='https://github.com/georgestarcher/Splunk-Class-httpevent',
       project_urls={
           'Source': 'https://github.com/georgestarcher/Splunk-Class-httpevent/blob/master/splunk_http_event_collector.py',
           'Example': 'https://github.com/georgestarcher/Splunk-Class-httpevent/blob/master/example.py',},
