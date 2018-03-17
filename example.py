c9a97c5ab3eb4f3ee18d1fc37ac92a3e07bcbc61
George Starcher <george@georgestarcher.com>
2018-03-16 21:58:48 -0500

    update readme markdown

--- example.py
+++ example.py
@@ -12,14 +12,14 @@ def commitCrime():
     return {"killer":random.choice(suspects), "weapon":random.choice(weapons), "location":random.choice(rooms), "victim":"Mr Boddy"}
 
 # Create event collector object, default SSL and HTTP Event Collector Port
-http_event_collector_key = "B02336E2-EEC2-48FF-9FA8-267B553A0C6B"
+http_event_collector_key = "4D14F8D9-D788-4E6E-BF2D-D1A46441242E"
 http_event_collector_host = "localhost"
 
 testevent = http_event_collector(http_event_collector_key, http_event_collector_host)
 
 # Start event payload and add the metadata information
 payload = {}
-payload.update({"index":"temp"})
+payload.update({"index":"test"})
 payload.update({"sourcetype":"crime"})
 payload.update({"source":"witness"})
 payload.update({"host":"mansion"})
